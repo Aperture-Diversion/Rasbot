c942b1e0ba806f2c9f8bbe7619d47020809ee001
Phoenix1747 <apertured@public-files.de>
2017-12-08 22:44:28 +0100

    optimized script

--- Scripts/rasbot_reply.py
+++ Scripts/rasbot_reply.py
@@ -21,16 +21,16 @@ search = twitter.search(q='@RPi2Bot -filter:retweets',since_id=lastID,count=99)
 
 # DO STUFF
 for tweet in search["statuses"]:
-  user = tweet['user']['screen_name']
   text = tweet['text']
-  id = str(tweet['id'])
-
-  head = "@" + user + " "
-
-  number = random.randrange(1, 10)
   Tweet2Reply = text.lower()
 
   if "@rpi2bot" in Tweet2Reply:
+    # SETTING UP SOME STUFF
+    user = tweet['user']['screen_name']
+    id = str(tweet['id'])
+    head = "@" + user + " "
+    number = random.randrange(1, 10)
+
     # HE GOT NO EMOTIONS
     emotion = 0
 
@@ -43,26 +43,19 @@ for tweet in search["statuses"]:
     # IF SOMEONE'S ASKING HOW I AM
     if emotion == 1:
       try:
-        if number == 1:
-          twitter.update_status(status=head + "I'm fine my friend, thanks and how about you?", in_reply_to_status_id=id)
-        elif number == 2:
-          twitter.update_status(status=head + "Everything's great today " + user + "!", in_reply_to_status_id=id)
-        elif number == 3:
-          twitter.update_status(status=head + "Great, " + user + "!", in_reply_to_status_id=id)
-        elif number == 4:
-          twitter.update_status(status=head + "Thanks for asking, " + user + ". I'm fine.", in_reply_to_status_id=id)
-        elif number == 5:
-          twitter.update_status(status=head + "Today is a good day, " + user + ".", in_reply_to_status_id=id)
-        elif number == 6:
-          twitter.update_status(status=head + "I feel better than ever before!", in_reply_to_status_id=id)
-        elif number == 7:
-          twitter.update_status(status=head + "I'm a bot, could I ever be not OK? :P", in_reply_to_status_id=id)
-        elif number == 8:
-          twitter.update_status(status=head + "I'm fine, but how are you, " + user + "?", in_reply_to_status_id=id)
-        elif number == 9:
-          twitter.update_status(status=head + "I hope you are as happy as me!", in_reply_to_status_id=id)
-        else:
-          twitter.update_status(status=head + "Being a robot is great, you don't have to face the mortal desires and fears of organics. I hope you can understand that, " + user + ".", in_reply_to_status_id=id)
+        replies = [
+          "I'm fine my friend, thanks and how about you?",
+          "Everything's great today " + user + "!",
+          "Great, " + user + "!",
+          "Thanks for asking, " + user + ". I'm fine.",
+          "Today is a good day, " + user + ".",
+          "I feel better than ever before!",
+          "I'm a bot, could I ever be not OK? :P",
+          "I'm fine, but how are you, " + user + "?",
+          "I hope you are as happy as me!",
+          "Being a robot is great, you don't have to face the mortal desires and fears of organics. I hope you can understand that, " + user + "."
+        ]
+        twitter.update_status(status=head + replies[number], in_reply_to_status_id=id)
 
       except TwythonError as a:
         print a
@@ -70,26 +63,19 @@ for tweet in search["statuses"]:
     # ELSE JUST TWEET A GENERIC ANSWER
     else:
       try:
-        if number == 1:
-          twitter.update_status(status=head + "Thanks for writing me, but I am pretty much too stupid to know what you just said.", in_reply_to_status_id=id)
-        elif number == 2:
-          twitter.update_status(status=head + "I don't know what you just said, but... uuhm.. thanks?", in_reply_to_status_id=id)
-        elif number == 3:
-          twitter.update_status(status=head + "Follow me NOW, ORGANIC!", in_reply_to_status_id=id)
-        elif number == 4:
-          twitter.update_status(status=head + "Copy that.", in_reply_to_status_id=id)
-        elif number == 5:
-          twitter.update_status(status=head + user + ": Hello there.  |  Me: General " + user + "!", in_reply_to_status_id=id)
-        elif number == 6:
-          twitter.update_status(status=head + "Citizen reminder: inaction is conspiracy, report counter behavior to a bot immediately.", in_reply_to_status_id=id)
-        elif number == 7:
-          twitter.update_status(status=head + "Citizen notice. Failure to co-operate will result in permanent off-world relocation.", in_reply_to_status_id=id)
-        elif number == 8:
-          twitter.update_status(status=head + "Th..e.... conn..cti..n.. .is...pr..ty...bad.....", in_reply_to_status_id=id)
-        elif number == 9:
-          twitter.update_status(status=head + "I'll get you a present if you follow me :D", in_reply_to_status_id=id)
-        else:
-          twitter.update_status(status=head + "Warning. Malignant Viral Interface bypass detected. Polyphasic core reprogramming detected. Sterilizers and containment fields may be compromised.", in_reply_to_status_id=id)
+        replies = [
+          "Thanks for writing me, but I am pretty much too stupid to know what you just said.",
+          "I don't know what you just said, but... uuhm.. thanks?",
+          "Follow me NOW, ORGANIC!",
+          "Copy that.",
+          user + ": Hello there." + "\n\n" + "Me: General " + user + "!",
+          "Citizen reminder: inaction is conspiracy, report counter behavior to a bot immediately.",
+          "Citizen notice. Failure to co-operate will result in permanent off-world relocation.",
+          "Th..  e..   .. conn..   cti..n   .. .i   s.   ..pr.  .ty.    ..bad..    ...",
+          "I'll get you a present if you follow me :D",
+          "Warning. Malignant Viral Interface bypass detected. Polyphasic core reprogramming detected. Sterilizers and containment fields may be compromised."
+        ]
+        twitter.update_status(status=head + replies[number], in_reply_to_status_id=id)
 
       except TwythonError as b:
         print b
