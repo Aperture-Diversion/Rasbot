6698a8583fca2490adbb062358f332227f77bca7
Phoenix1747 <apertured@public-files.de>
2017-12-09 14:10:54 +0100

    changed comment

--- Scripts/rasbot_tweet.py
+++ Scripts/rasbot_tweet.py
@@ -11,5 +11,5 @@ ACCESS_KEY = '*'
 ACCESS_SECRET = '*'
 twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
-# CREATE A SIMPLE TWEET
+# CREATE A SIMPLE TWEET USING: python rasbot_retweet.py 'YOUR_TEXT'
 twitter.update_status(status=sys.argv[1])
