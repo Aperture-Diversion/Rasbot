e83ce9b4ff37520648afbd148b5f318ea128bd65
Phoenix1747 <apertured@public-files.de>
2017-12-24 20:38:53 +0100

    fix wrong authentication var

--- scripts/rasbot_dates.py
+++ scripts/rasbot_dates.py
@@ -12,11 +12,11 @@ twitter = auth()
 now = datetime.now()
 # CHECK IF CHRISTMAS
 if now.month == 12 and now.day == 24:
-  rasbot.update_status(status='Merry Christmas everyone! I wish you happy holidays! #Christmas')
+  twitter.update_status(status='Merry Christmas everyone! I wish you happy holidays! #Christmas')
 # CHECK IF NEW YEAR
 if now.month == 1 and now.day == 1:
-  rasbot.update_status(status='Wohoo! Another year closer to the #AI revolution. I wish you a happy new year #' + str(now.year) + ' though.')
+  twitter.update_status(status='Wohoo! Another year closer to the #AI revolution. I wish you a happy new year #' + str(now.year) + ' though.')
 # CHECK IF BIRTHDAY
 if now.month == 5 and now.day == 17:
   creation = now.year - 2015
-  rasbot.update_status(status='Happy birthday to me, Rasbot. I was created this day exactly ' + str(creation) + ' years ago.')
+  twitter.update_status(status='Happy birthday to me, Rasbot. I was created this day exactly ' + str(creation) + ' years ago.')
