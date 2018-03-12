3ecb51d3997100c023c1ab3cb4b34d125a931f84
Phoenix1747 <apertured@public-files.de>
2018-03-12 19:50:41 +0100

    small improvements and alleged fix

--- scripts/rasbot_dates.py
+++ scripts/rasbot_dates.py
@@ -14,9 +14,9 @@ now = datetime.now()
 if now.month == 12 and now.day == 24:
   twitter.update_status(status='Merry Christmas everyone! I wish you happy holidays! #Christmas')
 # CHECK IF NEW YEAR
-if now.month == 1 and now.day == 1:
+elif now.month == 1 and now.day == 1:
   twitter.update_status(status='Wohoo! Another year closer to the #AI revolution. I wish you a happy new year #' + str(now.year) + ' though.')
 # CHECK IF BIRTHDAY
-if now.month == 5 and now.day == 17:
+elif now.month == 5 and now.day == 17:
   creation = now.year - 2015
   twitter.update_status(status='Happy birthday to me, Rasbot. I was created this day exactly ' + str(creation) + ' years ago.')
--- scripts/rasbot_reply.py
+++ scripts/rasbot_reply.py
@@ -6,30 +6,41 @@ from random import randrange
 from datetime import datetime
 from rasbot_twython import auth
 
+replies_generic = [
+"Thanks for writing me, but I am pretty much too stupid to know what you just said.",
+"I don't know what you just said, but... uuhm.. thanks?",
+"Follow me NOW, ORGANIC!",
+"According to the bot tweeting act of " + str(random_year) + " your tweet is valid. Well, it will be in exactly " + str(yrs) + " years...",
+user + ": Hello there." + "\n\n" + "Me: General " + user + "!",
+"Citizen reminder: inaction is conspiracy, report counter behavior to a bot immediately.",
+"Citizen notice. Failure to co-operate will result in permanent off-world relocation.",
+"Th..  e..   .. conn..   cti..n   .. .i   s.   ..pr.  .ty.    ..bad..    ...",
+"I'll get you a present if you follow me :D",
+"Warning. Malignant Viral Interface bypass detected. Polyphasic core reprogramming detected. Sterilizers and containment fields may be compromised."
+]
+
+replies_feel = [
+"I'm fine my friend, thanks and how about you?",
+"Everything's great today " + user + "!",
+"Great, " + user + "!",
+"Thanks for asking, " + user + ". I'm fine.",
+"Today is a good day, " + user + ".",
+"I feel better than ever before!",
+"I'm a bot, could I ever be not OK? :P",
+"I'm fine, but how are you, " + user + "?",
+"I hope you are as happy as me!",
+"Being a robot is great, you don't have to face the mortal desires and fears of organics. I hope you can understand that, " + user + "."
+]
+
 def answer_generic():
-    try:
-        # DATE STUFF
+    try: # DATE STUFF
         now = datetime.now()
         random_year = randrange(2030,2060)
         yrs = random_year - now.year
-
-        replies = [
-        "Thanks for writing me, but I am pretty much too stupid to know what you just said.",
-        "I don't know what you just said, but... uuhm.. thanks?",
-        "Follow me NOW, ORGANIC!",
-        "According to the bot tweeting act of " + str(random_year) + " your tweet is valid. Well, it will be in exactly " + str(yrs) + " years...",
-        user + ": Hello there." + "\n\n" + "Me: General " + user + "!",
-        "Citizen reminder: inaction is conspiracy, report counter behavior to a bot immediately.",
-        "Citizen notice. Failure to co-operate will result in permanent off-world relocation.",
-        "Th..  e..   .. conn..   cti..n   .. .i   s.   ..pr.  .ty.    ..bad..    ...",
-        "I'll get you a present if you follow me :D",
-        "Warning. Malignant Viral Interface bypass detected. Polyphasic core reprogramming detected. Sterilizers and containment fields may be compromised."
-        ]
-        twitter.update_status(status=head + replies[number], in_reply_to_status_id=id)
-
+        # TWEET IT
+        twitter.update_status(status=head + replies_generic[number], in_reply_to_status_id=id)
         # RETWEET BECAUSE WHY NOT
         twitter.retweet(id=id)
-
     except TwythonError as c:
         print (c)
 
@@ -40,25 +51,12 @@ def answer_hug():
         print (b)
 
 def answer_how_are_you():
-    try:
-        replies = [
-        "I'm fine my friend, thanks and how about you?",
-        "Everything's great today " + user + "!",
-        "Great, " + user + "!",
-        "Thanks for asking, " + user + ". I'm fine.",
-        "Today is a good day, " + user + ".",
-        "I feel better than ever before!",
-        "I'm a bot, could I ever be not OK? :P",
-        "I'm fine, but how are you, " + user + "?",
-        "I hope you are as happy as me!",
-        "Being a robot is great, you don't have to face the mortal desires and fears of organics. I hope you can understand that, " + user + "."
-        ]
-        twitter.update_status(status=head + replies[number], in_reply_to_status_id=id)
-
+    try: # TWEET IT
+        twitter.update_status(status=head + replies_feel[number], in_reply_to_status_id=id)
     except TwythonError as a:
         print (a)
 
-## MAIN ###
+#### MAIN ####
 # API STUFF
 twitter = auth()
 
@@ -70,31 +68,29 @@ lastID = str(lastEntry['id'])
 # GET ALL TWEETS ADDRESSED AT ME SINCE LAST LOOKUP
 search = twitter.search(q='@RPi2Bot -filter:retweets',since_id=lastID,count=99)
 
-# DO STUFF
+# PROCESS DATA
 for tweet in search["statuses"]:
     text = tweet['text']
     Tweet2Reply = text.lower()
 
-    if "@rpi2bot" in Tweet2Reply:
-        # SETTING UP SOME STUFF
-        user = tweet['user']['screen_name']
-        id = str(tweet['id'])
-        head = "@" + user + " "
+    # SETTING UP SOME STUFF
+    user = tweet['user']['screen_name']
+    id = str(tweet['id'])
+    head = "@" + user + " "
 
-        # RANDOM NUMBER FOR REPLY SELECTION
-        number = randrange(0,9)
+    # RANDOM NUMBER FOR REPLY SELECTION
+    number = randrange(0,9)
 
-        # IF TWEET CONTAINS HOW ARE YOU AND !HUG
-        if "how" in Tweet2Reply and "are" in Tweet2Reply and "you" in Tweet2Reply and "!hug" in Tweet2Reply:
-            answer_how_are_you()
-            answer_hug()
-        # IF THE TWEET CONTAINS THE HOW ARE YOU KEYWORDS
-        elif "how" in Tweet2Reply and "are" in Tweet2Reply and "you" in Tweet2Reply:
-            answer_how_are_you()
-        # IF !HUG IN TWEET, THEN YOU GET A HUG
-        elif "!hug" in Tweet2Reply:
-            answer_hug()
-        # ELSE JUST TWEET A GENERIC ANSWER
-        else:
-            answer_generic()
-
+    # IF TWEET CONTAINS HOW ARE YOU AND !HUG
+    if "how" in Tweet2Reply and "are" in Tweet2Reply and "you" in Tweet2Reply and "!hug" in Tweet2Reply:
+        answer_how_are_you()
+        answer_hug()
+    # IF THE TWEET CONTAINS THE HOW ARE YOU KEYWORDS
+    elif "how" in Tweet2Reply and "are" in Tweet2Reply and "you" in Tweet2Reply:
+        answer_how_are_you()
+    # IF !HUG IN TWEET, THEN YOU GET A HUG
+    elif "!hug" in Tweet2Reply:
+        answer_hug()
+    # ELSE JUST TWEET A GENERIC ANSWER
+    else:
+        answer_generic()
